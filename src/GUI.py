3661ed0593b97fc9651ee6db72c8beca6a6e7749
Marslel <marcel.bergen123@gmail.com>
2023-05-01 17:23:02 +0200

    Commit -4: Bug fix und Kommentare wurden hinzugefügt

--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,8 @@
+"""
+In dieser Klasse wird die GUI erstellt, um das Kartenspiel Nope zu spielen
+
+@author Marcel Bergen
+"""
 import tkinter
 import tkinter as tk
 from tkinter import messagebox
@@ -18,20 +23,21 @@ class MainWindow(tk.Tk):
 
         self.frames = {}
 
-        print("test")
+        #Hier werden alle Frames in einem Container gepackt
         for F in (Login, Register, Game):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        print("test1")
+        #Diese Methode ruft bei dem Programmstart den Login Screen zuerst auf
         self.showFrame(Login)
-        print("test2")
 
+    #Diese Methode schaut nach ob ein bereits registrierter User sich anmeldet
     def login(self, usernameEntry, passwordEntry):
         username = "Marsle"
         password = "Software"
         if usernameEntry.get() == username and passwordEntry.get() == password:
+            #Aus der Klasse Request wird die Login methode verwendet um den Access key zu erhalten
             access = Requests.login(username, password)
             print("Successfully logged in")
             print(access.text)
@@ -39,22 +45,22 @@ class MainWindow(tk.Tk):
         else:
             messagebox.showerror(title="Error", message="Failed to login.")
 
+    #In dieser Methode kann sich ein neuer Benutzer sich registrieren
     def register(self, username, password, firstName, lastName):
         Requests.register(username, password, firstName, lastName)
         messagebox.showinfo(title="Success", message="New player created")
         self.showFrame(Login)
 
+    #Diese Methode zeigt den aktuellen Frame
     def showFrame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-
+#Diese Klasse ist der Haupt Screen und man kann sich hier einloggen
 class Login(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
-        print("hallo")
 
         # Widgets erstellen
         loginLabel = tkinter.Label(self, text="Login", bg='#878787', fg='#FFFFFF', font=("Arial", 30))
@@ -64,92 +70,66 @@ class Login(tk.Frame):
         passwordEntry = tkinter.Entry(self, show="*", font=("Arial", 30))
         loginButton = tkinter.Button(self, text="Login", bg='#666666', fg='#FFFFFF', font=("Arial", 30),
                                      command=lambda: controller.login(usernameEntry, passwordEntry))
-        registerButton = tkinter.Button(self, text="Register", bg='#666666', fg='#FFFFFF', font=("Arial", 20),
+        registerButton = tkinter.Button(self, text="Register", bg='#666666', fg='#FFFFFF', font=("Arial", 17),
                                         command=lambda: controller.showFrame(Register))
 
-        loginLabel.pack()
-        usernameLabel.pack()
-        usernameEntry.pack()
-        passwordLabel.pack()
-        passwordEntry.pack()
-        loginButton.pack()
-        registerButton.pack()
-
         # Widgets in der richtigen Position bringen
-        '''
-        loginLabel.grid(row=0, column=0, columnspan=5, sticky="news", pady=40)
+        loginLabel.grid(row=0, column=0, columnspan=60, sticky="news", pady=40)
         usernameLabel.grid(row=1, column=0)
         usernameEntry.grid(row=1, column=1, pady=20)
         passwordLabel.grid(row=2, column=0)
         passwordEntry.grid(row=2, column=1, pady=20)
         loginButton.grid(row=3, column=0, columnspan=5, pady=30)
-        registerButton.grid(row=4, column=3, columnspan=5, pady=5)
-        '''
+        registerButton.grid(row=4, column=2, columnspan=1, pady=5)
 
-
+#Mit dieser Klasse können sich neue Benutzer sich registrieren
 class Register(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        print("hallo1")
         # Widgets erstellen
-        registerLabel = tkinter.Label(self, text="Login", bg='#878787', fg='#FFFFFF', font=("Arial", 30))
+        registerLabel = tkinter.Label(self, text="Register", bg='#878787', fg='#FFFFFF', font=("Arial", 30))
         usernameLabel = tkinter.Label(self, text="Username", bg='#878787', fg='#FFFFFF', font=("Arial", 16))
         usernameEntry = tkinter.Entry(self, font=("Arial", 30))
         passwordLabel = tkinter.Label(self, text="Password", bg='#878787', fg='#FFFFFF', font=("Arial", 16))
         passwordEntry = tkinter.Entry(self, show="*", font=("Arial", 30))
         firstNameLabel = tkinter.Label(self, text="Firstname", bg='#878787', fg='#FFFFFF', font=("Arial", 16))
-        firstNameEntry = tkinter.Entry(self, show="*", font=("Arial", 30))
+        firstNameEntry = tkinter.Entry(self, font=("Arial", 30))
         lastNameLabel = tkinter.Label(self, text="Lastname", bg='#878787', fg='#FFFFFF', font=("Arial", 16))
-        lastNameEntry = tkinter.Entry(self, show="*", font=("Arial", 30))
-        RegisterButton = tkinter.Button(self, text="Login", bg='#666666', fg='#FFFFFF', font=("Arial", 30),
+        lastNameEntry = tkinter.Entry(self, font=("Arial", 30))
+        RegisterButton = tkinter.Button(self, text="Register", bg='#666666', fg='#FFFFFF', font=("Arial", 30),
                                         command=lambda: controller.register(usernameEntry, passwordEntry,
                                                                             firstNameEntry, lastNameEntry))
-        '''
+
         # Widgets in der richtigen Position bringen
-        registerLabel.grid(row=0, column=0, columnspan=5, sticky="news", pady=40)
+        registerLabel.grid(row=0, column=1, columnspan=5, sticky="news", padx=190, pady=20)
         usernameLabel.grid(row=1, column=0)
-        usernameEntry.grid(row=1, column=1, pady=20)
+        usernameEntry.grid(row=1, column=1, pady=10)
         passwordLabel.grid(row=2, column=0)
-        passwordEntry.grid(row=2, column=1, pady=20)
+        passwordEntry.grid(row=2, column=1, pady=10)
         firstNameLabel.grid(row=3, column=0)
-        firstNameEntry.grid(row=3, column=1, pady=20)
+        firstNameEntry.grid(row=3, column=1, pady=10)
         lastNameLabel.grid(row=4, column=0)
-        lastNameEntry.grid(row=4, column=1, pady=20)
-        RegisterButton.grid(row=5, column=0, columnspan=5, pady=30)
-        '''
-        registerLabel.pack()
-        usernameLabel.pack()
-        usernameEntry.pack()
-        passwordLabel.pack()
-        passwordEntry.pack()
-        firstNameLabel.pack()
-        firstNameEntry.pack()
-        lastNameLabel.pack()
-        lastNameEntry.pack()
-        RegisterButton.pack()
+        lastNameEntry.grid(row=4, column=1, pady=10)
+        RegisterButton.grid(row=5, column=1, padx=180, pady=30)
 
-
+#Diese Klasse spiegelt den Hauptscreen des Spieles wieder
 class Game(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        print("hallo2")
         gameLabel = tkinter.Label(self, text="Wellcome to the Game", bg='#878787', fg='#FFFFFF', font=("Arial", 30))
 
         titlescreenButton = tkinter.Button(self, text="Titelscreen", bg='#666666', fg='#FFFFFF', font=("Arial", 30),
                                            command=lambda: controller.showFrame(Login))
 
-        '''
-        gameLabel.grid(row=0, column=0, columnspan=5, sticky="news", pady=40)
-        titlescreenButton.grid(row=5, column=0, columnspan=5, pady=30)
-        '''
-
-        gameLabel.pack()
-        titlescreenButton.pack()
+        gameLabel.grid(row=0, column=2, columnspan=5, sticky="news", pady=20)
+        titlescreenButton.grid(row=500, column=6, padx=250, pady=290)
 
 
 window = MainWindow()
+window.title("Cardgame-Nope")
+window.geometry("680x520")
 window.mainloop()
