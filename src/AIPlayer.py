3253f5fcd31eeae561083b0f22c9f96ad88a834f
Marslel <marcel.bergen123@gmail.com>
2023-06-06 09:29:49 +0200

    Commit -11: Bug Fix

--- src/AIPlayer.py
+++ src/AIPlayer.py
@@ -24,8 +24,16 @@ class Move:
         self.card3 = card3
         self.reason = reason
 
+isTake = False
+takeCard = 0
+takeValue = 0
+takeCardValue = 0
 
 def play_nope(state):
+    global isTake
+    global takeCard
+    global takeValue
+    global takeCardValue
     players = state['players']
     currentPlayerIDx = state['currentPlayerIdx']
     currentPlayer = players[currentPlayerIDx]
@@ -41,45 +49,85 @@ def play_nope(state):
 
     # Überprüfen, ob currentPlayerIdx unserer ID entspricht
     if currentPlayer['username'] == "Marsle":
-        print(f"\n{currentPlayer['username']}'s turn:")
+        if not isTake:
+            print(f"\n{currentPlayer['username']}'s turn:")
 
-        if currentCard:
-            print(f"Current card: {currentCard}")
+            if currentCard:
+                print(f"Current card: {currentCard}")
 
-        value = 0
-        index = []
-        cardValue = int(currentCard['value'])
-        for i in range(handSize):
-            if hands[i]['color'] == currentCard['color']:
-                color = hands[i]['color']
-                print(f"\n{color}")
-                index.append(i)
-                value += 1
+            value = 0
+            index = []
+            cardValue = int(currentCard['value'])
+            for i in range(handSize):
+                if hands[i]['color'] == currentCard['color']:
+                    color = hands[i]['color']
+                    print(f"\n{color}")
+                    index.append(i)
+                    value += 1
 
-        value = 3
-        if value >= cardValue:
-            if value >= 3:
-                thirdCard.remove(0)
-                thirdCard = hands[index[2]]
+            #Wenn man genug Karten hat, dann können die rausgelegt werden
+            if value >= cardValue:
+                if value >= 3:
+                    thirdCard.remove(0)
+                    thirdCard = hands[index[2]]
 
-                value -= 1
+                    value -= 1
 
-            if value >= 2:
-                secondCard.remove(0)
-                secondCard.append(hands[index[1]])
-                value -= 1
+                if value >= 2:
+                    secondCard.remove(0)
+                    secondCard.append(hands[index[1]])
+                    value -= 1
 
-            if value >= 1:
-                firstCard.remove(0)
-                firstCard.append(hands[index[0]])
+                if value >= 1:
+                    firstCard.remove(0)
+                    firstCard.append(hands[index[0]])
 
-            type.append("put")
-            moveData = Move(type[0], firstCard[0], secondCard[0], thirdCard, 'reason_value')
+                type.append("put")
+                moveData = Move(type[0], firstCard[0], secondCard[0], thirdCard, 'reason_value')
+            #Meine Karten Anzahl ist zu gering
+            else:
+                type.append("take")
+                isTake = True
+                takeCard = currentCard['color']
+                takeValue = value
+                takeCardValue = cardValue
+                moveData = Move(type[0], firstCard[0], secondCard[0], thirdCard[0], 'reason_value')
+        # Nope Fall
         else:
-            type.append("take")
-            moveData = Move(type[0], firstCard[0], secondCard[0], thirdCard[0], 'reason_value')
+            index = []
+            #Hier wird geschaut ob die neu gezogene Karte die richtige Farbe hat
+            if hands[handSize-1]['color'] == takeCard:
+                takeValue +=1
+                for i in range(handSize):
+                    if hands[i]['color'] == takeCard:
+                        index.append(i)
+                # Wenn man genug Karten hat, dann können die rausgelegt werden
+                if takeValue == takeCardValue:
+                    if takeValue >= 3:
+                        thirdCard.remove(0)
+                        thirdCard = hands[index[2]]
 
+                        takeValue -= 1
 
+                    if takeValue >= 2:
+                        secondCard.remove(0)
+                        secondCard.append(hands[index[1]])
+                        takeValue -= 1
+
+                    if takeValue >= 1:
+                        firstCard.remove(0)
+                        firstCard.append(hands[index[0]])
+
+                    type.append("put")
+                    moveData = Move(type[0], firstCard[0], secondCard[0], thirdCard, 'reason_value')
+                #Sonst wird Nope gespielt
+                else:
+                    print("Hier Nope")
+                    moveData = Move("nope", 0, 0, 0, 'Use nope')
+            #Sonst wird Nope gespielt
+            else:
+                print("Oder Hier Nope")
+                moveData = Move("nope", 0, 0, 0, 'Use nope')
 
 
         # JSON-Datei schreiben
@@ -93,6 +141,3 @@ def play_nope(state):
 
     else:
         print("It's not our turn.")
-
-
-play_nope(state)
